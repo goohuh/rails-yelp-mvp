f59eb4ac5ba14b3197a32e68e4cee94430add08e
Arnaud Legaignoux <arnaud.legaignoux@gmail.com>
2023-08-16 14:29:53 +0200

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,34 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Rails.application.routes.draw do
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+#   # Defines the root path route ("/")
+#   # root "articles#index"
+#   get 'restaurants', to: 'restaurants#index'
+
+#   get 'restaurants/new', to: 'restaurants#new'
+#   post 'restaurants', to: 'restaurants#create'
+
+#   get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+#   get '/restaurants/:id/reviews/new',
+# end
+
+# Rails.application.routes.draw do
+#   resources :restaurants do
+#     resources :reviews, only: [:new, :create]
+#   end
+#   resources :reviews, only: [:show, :edit, :update, :destroy]
+# end
+
+Rails.application.routes.draw do
+  resources :restaurants, { shallow: true } do
+    resources :reviews
+  end
 end
+
+# Rails.application.routes.draw do
+#   resources :restaurants do
+#     resources :reviews, only: [:index, :new, :create]
+#   end
+#   resources :reviews, only: [:show, :edit, :update, :destroy]
+# end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+Review.destroy_all
+
+Restaurant.create(name: 'Les Fondus de Fondue', address: '3 rue du Nil, 75015 Paris', category: 'french')
+Restaurant.create(name: 'Happy Sushis', address: '18 impasse des tilleuls, Strasbourg', category: 'japanese')
+Restaurant.create(name: 'Bellissima', address: '12 allée des peupliers, Maubeuge', category: 'italian')
+Restaurant.create(name: 'Le Lotus', address: '25 place des fêtes, Neuilly', category: 'chinese')
+Restaurant.create(name: 'The Big Waffle', address: '5 place de la Bourse, Liège', category: 'belgian')
