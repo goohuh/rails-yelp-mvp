0939557a5ee00fb104111cf320cc5b85241a60ca
Arnaud Legaignoux <arnaud.legaignoux@gmail.com>
2023-08-16 15:50:17 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@
 
 Rails.application.routes.draw do
   resources :restaurants, { shallow: true } do
-    resources :reviews
+    resources :reviews, except: [:new]
   end
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,17 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 Restaurant.destroy_all
+puts 'Restaurants database deleted'
 Review.destroy_all
+puts 'Reviews database deleted'
 
-Restaurant.create(name: 'Les Fondus de Fondue', address: '3 rue du Nil, 75015 Paris', category: 'french')
-Restaurant.create(name: 'Happy Sushis', address: '18 impasse des tilleuls, Strasbourg', category: 'japanese')
-Restaurant.create(name: 'Bellissima', address: '12 allée des peupliers, Maubeuge', category: 'italian')
-Restaurant.create(name: 'Le Lotus', address: '25 place des fêtes, Neuilly', category: 'chinese')
-Restaurant.create(name: 'The Big Waffle', address: '5 place de la Bourse, Liège', category: 'belgian')
+Restaurant.create(name: 'Les Fondus de Fondue', address: '3 rue du Nil, 75015 Paris', phone_number: '01 45 33 65 72', category: 'french')
+puts 'Restaurant #1 created'
+Restaurant.create(name: 'Happy Sushis', address: '18 impasse des tilleuls, Strasbourg', phone_number: '01 45 33 65 72', category: 'japanese')
+puts 'Restaurant #2 created'
+Restaurant.create(name: 'Bellissima', address: '12 allée des peupliers, Maubeuge', phone_number: '01 45 33 65 72', category: 'italian')
+puts 'Restaurant #3 created'
+Restaurant.create(name: 'Le Lotus', address: '25 place des fêtes, Neuilly', phone_number: '01 45 33 65 72', category: 'chinese')
+puts 'Restaurant #4 created'
+Restaurant.create(name: 'The Big Waffle', address: '5 place de la Bourse, Liège', phone_number: '01 45 33 65 72', category: 'belgian')
+puts 'Restaurant #5 created'
